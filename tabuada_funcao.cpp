584f2fa1be80170f9e4c8ec1693d03b159f4b625
Fernando <fernandomaioranicosta6@gmail.com>
2026-01-30 21:16:35 -0300

    Gerador de tabuada utilizando void

---
+++ tabuada_funcao.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+void tabuada(int n)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        std::cout << n << " x " << i << " = " << n * i << std::endl;
+    }
+}
+
+int main ()
+{
+    int n;
+    std::cout << " Digite um numero para ver a sua tabuada : ";
+    std::cin>> n;
+    tabuada(n);
+   std::cout << " Essa foi a tabuada do numero " <<  n  <<  "." << " Espero que tenha gostado!" << std::endl;
+    return 0;
+}
